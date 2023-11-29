b2104e53b505feb6d1563391f79785b33a55eb96
kuriroley <christian.rodriguez@tutanota.com>
2023-11-29 13:48:15 +0900

    changes

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 //variables... probably to use later
-let displayValue = 0;
-let value1 = 0;
-let value2 = 0;
+let operand1 = 0;
+let operand2 = 0;
 let operator = '';
 
+
+//functions 
+    //operations
+function add(a,b){return a + b}
+function substract(a,b){return a - b}
+function multiply(a,b){return a * b}
+function divide(a,b){return a / b}
+function rootSquare(a){return Math.sqrt(a)}
+
+    //main operate
+function operate(a,operator,b){
+    if(operator == '+'){
+        return add(a,b);
+    }
+    if(operator == '-'){
+        return substract(a,b);
+    }
+    if(operator == '*'){
+        return multiply(a,b);
+    }
+    if(operator == '/'){
+        return divide(a,b);
+    }
+    if(operator == 'v'){
+        return rootSquare(a)
+    }
+}
 //dom elements
 const display = document.querySelector('.display')
 const clearButton = document.querySelector('.clear');
 const buttonContainer = document.querySelector('.buttons-container');
-const numberButtons = document.querySelectorAll('.number');
-
+const operand = document.querySelectorAll('.operand')
+const log = document.querySelector('.log');
 //event listeners
 
+
+
+
 //using event delegation for buttons
-buttonContainer.addEventListener('click', (e)=>{
-    if(e.target && e.target.matches == 'button.number'){
-        console.log('number clicked');
-    }
-})
-
-function doSomething(){
-    alert('aaaa');
-}
-
-
-//events for number buttons
-
+//could that work?
 
 
 //for clearing button
@@ -42,6 +59,8 @@ clearButton.addEventListener('click',()=>{
 
 
 
+//for a history
+log.textContent = 'History tracker: ////'
 
 
 
@@ -52,38 +71,7 @@ clearButton.addEventListener('click',()=>{
 
 // functions for calculator
 
-// function add(a,b){
-//     return a + b;
-// }
-// function substract(a,b){
-//     return a - b;
-// }
-// function multiply(a,b){
-//     return a * b;
-// }
-// function divide(a,b){
-//     return a / b;
-// }
-// function rootSquare(a){
-//     return Math.sqrt(a);
-// }
+
 
 //what if I pass this function as a high-order function?
 
-// function operate(a,operator,b){
-//     if(operator == '+'){
-//         return add(a,b);
-//     }
-//     if(operator == '-'){
-//         return substract(a,b);
-//     }
-//     if(operator == '*'){
-//         return multiply(a,b);
-//     }
-//     if(operator == '/'){
-//         return divide(a,b);
-//     }
-//     if(operator == 'v'){
-//         return rootSquare(a)
-//     }
-// }
