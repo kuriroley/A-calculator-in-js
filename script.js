5fe782cec64547815c97cfe328760da2e0b53c4e
kuriroley <christian.rodriguez@tutanota.com>
2023-12-01 15:29:18 +0900

    changes

--- script.js
+++ script.js
@@ -1,107 +1,74 @@
-//variables... probably to use later
-let operand1 = 0;
-let operand2 = 0;
-let operator = '';
-
-//functions 
-    //operations
-function add(a,b){return a + b}
-function substract(a,b){return a - b}
-function multiply(a,b){return a * b}
-function divide(a,b){return a / b}
-function rootSquare(a){return Math.sqrt(a)}
-
-    //main operate function
-function operate(a,operator,b){
-    if(operator == '+'){
-        return add(a,b);
-    }
-    if(operator == '-'){
-        return substract(a,b);
-    }
-    if(operator == '*'){
-        return multiply(a,b);
-    }
-    if(operator == '/'){
-        return divide(a,b);
-    }
-    if(operator == 'v'){
-        return rootSquare(a)
-    }
-}
 //dom elements
 const display = document.querySelector('.display')
 const clearButton = document.querySelector('.clear');
 const buttonContainer = document.querySelector('.buttons-container');
-const operand = document.querySelectorAll('.operand');
+const operands = document.querySelectorAll('.operand');
 const operators = document.querySelectorAll('.operator');
-const log = document.querySelector('.log');
+
+//variables... probably to use later
+
 
 
 //event listeners
-//first forEach and then the eventlistener 
-operand.forEach((number)=>{
-    number.addEventListener('click', ()=>{
-        display.value = number.value
+// functions to populate display
 
+//function for getting the first operand;
+
+
+//for getting the first
+operands.forEach((operand)=>{
+    let displayValue = display.textContent;
+    let operand1;
+    let operand2;
+    let operator;
+    operand.addEventListener('click', (e)=>{
+        operand1 += e.target.value;
+        display.textContent = operand1;
+    });
+
+
+    operand.addEventListener('click', (e)=>{
+        operand2 += e.target.value;
+        display.textContent = operand2;
+    });
+
+    operators.forEach((operator)=>{
+        operator.addEventListener('click', (e)=>{
+            if(operator.value == '+'){
+                display.textContent =+ add(2,3);
+            }
+        })
     })
-});
-//for operators (test)
-operators.forEach((operator)=>{
-    let op = '';
-    operator.addEventListener('click',()=>{
-        if(operator.value == '='){
-            op = operator.value;
-        }
-        if(operator.value == '+'){
-            op = operator.value;
-        }
-        if(operator.value == '*'){
-            op = operator.value;
-        }
-        if(operator.value == '-'){
-            op = operator.value;
-        }
+    clearButton.addEventListener('click', ()=>{
+        display.textContent = displayValue;
     })
 })
 
 
+//first forEach and then the eventlistener 
 
 
-
-
-
-//using event delegation for buttons
-//could that work?
-
-
-//for clearing button
-clearButton.addEventListener('click',()=>{
-    display.value= '';
-});
-
-
-
-
-
-
-
-
-
-
-//for a history
-log.textContent = 'History tracker: ////'
-
-
-
-
-
-
-
-
-// functions for calculator
-
-
-
-//what if I pass this function as a high-order function?
-
+//functions 
+        //main operate function
+    function operate(a,operator,b){
+        if(operator == '+'){
+            return add(a,b);
+        }
+        if(operator == '-'){
+            return substract(a,b);
+        }
+        if(operator == '*'){
+            return multiply(a,b);
+        }
+        if(operator == '/'){
+            return divide(a,b);
+        }
+        if(operator == 'v'){
+            return rootSquare(a)
+        }
+        function add(a,b){return a + b}
+        function substract(a,b){return a - b}
+        function multiply(a,b){return a * b}
+        function divide(a,b){return a / b}
+        function rootSquare(a){return Math.sqrt(a)}
+    }
